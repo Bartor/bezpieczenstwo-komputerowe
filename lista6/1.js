1f097d6b33cbf2f269a8eec54746a988bccdbf39
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-12-01 20:26:20 +0100

    wersja serio coś jednak odkodowująca

--- lista6/1.js
+++ lista6/1.js
@@ -1,31 +1,5 @@
 const fs = require('fs');
 
-const lettersFreq = {
-    a: 891,
-    b: 147,
-    c: 396,
-    d: 325,
-    e: 766,
-    f: 30,
-    g: 142,
-    h: 108,
-    i: 821,
-    j: 228,
-    k: 351,
-    l: 210,
-    m: 280,
-    n: 552,
-    o: 775,
-    p: 313,
-    r: 469,
-    s: 432,
-    t: 398,
-    u: 250,
-    w: 465,
-    y: 376,
-    z: 564
-};
-
 function stringXOR(a, b) {
     return a.split('').map((c, i) => c === b[i] ? '0' : '1').join('');
 }
@@ -38,6 +12,7 @@ function binaryToChar(binary) {
     return String.fromCharCode(Number.parseInt(binary, 2));
 }
 
+const letters = /[a-zA-Z]/;
 
 const cryptograms = fs.readFileSync('encoded.txt', {
         encoding: 'utf-8'
@@ -51,27 +26,26 @@ for (let i = 0; i < cryptograms.length; i++) {
     let cryptogramA = cryptograms[i];
     let possibleSpaces = [];
 
-    for (let j = i + 1; j < cryptograms.length; j++) {
+    for (let j = 0; j < cryptograms.length; j++) {
+        if (i === j) continue;
+
         let cryptogramB = cryptograms[j];
         let xoredCryptogram = [];
 
         let possibleSpacesWithB = [];
-        for (let k = 0; k < cryptogramB.length; k++) {
+        for (let k = 0; k < (cryptogramB.length > cryptogramA.length ? cryptogramA.length : cryptogramB.length); k++) {
             let xor = stringXOR(cryptogramA[k], cryptogramB[k]);
             xoredCryptogram[k] = xor;
-            if (xor.startsWith('01')) {
+            if ((xor.startsWith('01') && letters.test(binaryToChar(stringXOR(xor, charToBinary(' '))))) || xor === '00000000') {
                 possibleSpacesWithB.push(k);
             }
         }
         possibleSpaces.push(possibleSpacesWithB);
     }
 
-
     let intersection = possibleSpaces.reduce((acc, withB) => acc.filter(e => withB.indexOf(e) !== -1), possibleSpaces[0]);
     spacesIntersections.push(intersection ? intersection : []);
 }
-
-console.log(spacesIntersections);
 
 let secretKey = [];
 for (let i = 0; i < spacesIntersections.length; i++) {
@@ -79,18 +53,17 @@ for (let i = 0; i < spacesIntersections.length; i++) {
         secretKey[spacesIntersections[i][j]] = stringXOR(charToBinary(' '), cryptograms[i][spacesIntersections[i][j]]);
     }
 }
-console.log(secretKey.join(' '));
 
+console.log(`DECODED SECRET KEY: ${[...secretKey].map(e => e ? e : '________').join('|')}`);
 for (let i = 0; i < cryptograms.length; i++) {
     let decipheredCryptogram = [];
     for (let j = 0; j < secretKey.length; j++) {
         if (secretKey[j]) {
             decipheredCryptogram[j] = stringXOR(secretKey[j], cryptograms[i][j]);
         } else {
-            decipheredCryptogram[j] = '00111111';
+            decipheredCryptogram[j] = charToBinary('_');
         }
     }
 
     console.log(decipheredCryptogram.map(char => binaryToChar(char)).join(''));
-    console.log();
 }
